95a28e9cc2f38a44dfc8034c62c11c045a4872a6
Andrew Semchism <andrew0187@gmail.com>
2023-02-20 21:41:59 -0500

    Adjustments for home page for smaller screen sizes.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,13 +41,13 @@ const IndexPage: React.FC<PageProps> = () => {
       <main>
         <Container fluid className="title-content">
           <Row className="align-items-center">
-            <Col xs={0} lg={1} xl={2}></Col>
-            <Col lg={7} xl={6}>
+            <Col xs={0} md={0} lg={1} xl={2}></Col>
+            <Col xs={12} md={8} lg={7} xl={6}>
               <div className="title-text">
                 <h2>Hi, my name is</h2>
                 <h1>Andrew Semchism</h1>
                 <h2>I love software engineering.</h2>
-                <p>I am a third year Computer Science student at the University of Waterloo. I am currently completing an internship a Geotab.</p>
+                <p>I am a third year Computer Science student studying at the University of Waterloo. I am currently completing an internship a Geotab.</p>
                 <Button variant="outline-secondary">Contact Me</Button>
                 <div className="icons">
                   <a href="https://github.com/andrewsemchism" target="_blank"><FontAwesomeIcon icon={faGithub} size="2xl"/></a>
@@ -55,10 +55,10 @@ const IndexPage: React.FC<PageProps> = () => {
                 </div>
               </div>
             </Col>
-            <Col xs={0} lg={3} xl={2}>
+            <Col xs={0} md={4} lg={3} xl={2} className="d-none d-md-block">
               <StaticImage alt="Cartoon of Andrew" src="../images/andrew-cartoon.jpg"/>
             </Col>
-            <Col lg={1} xl={2}></Col>
+            <Col xs={0} md={0} lg={1} xl={2}></Col>
           </Row>
         </Container>
 
@@ -70,55 +70,6 @@ const IndexPage: React.FC<PageProps> = () => {
         
       </main>
     </div>
-    /*
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-    */
   )
 }
 
